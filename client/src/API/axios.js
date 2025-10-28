3c5cbe8c3d8b1894ce6a8a059f949c02dc159577
Apostle <melekte96@gmail.com>
2025-10-28 02:01:31 -0500

    --

--- client/src/API/axios.js
+++ client/src/API/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5500/api",
+  baseURL: "https://evangadiforum-thkp.onrender.com" || "http://localhost:5500/api",
   timeout: 10000,
 });
 
